6b0e0ec0a2b9c1bb4898b2db91466b4587809a97
fdoromo <fdoromo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-07-18 23:08:09 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wp-twitter/trunk@136477 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin_scripts.php
@@ -0,0 +1,67 @@
+<?php include('inc/functions.php');?>
+/*** Character Counter ***/
+function textCounter(field, countfield, maxlimit) {
+if (field.value.length > maxlimit)
+field.value = field.value.substring(0, maxlimit);
+else 
+countfield.value = maxlimit - field.value.length;
+}
+
+/*** Reply/Retweet Buttons ***/
+jQuery(document).ready(function() {
+
+	jQuery(".reply").click(function() {
+		reply_to_status = jQuery(this).parents().filter('span.status').attr('id');
+		reply_to_user = jQuery('#'+reply_to_status+' a.name1').html();
+		jQuery('#tweet').val('@'+reply_to_user+' ');
+		jQuery('#in_reply_to_user').val(reply_to_user);
+		jQuery('#in_reply_to_status').val(reply_to_status);
+	});
+	
+	jQuery('#tweet').keyup(function(){
+		var length = jQuery('#tweet').val().length;
+		if (length == 0) {
+			jQuery('#in_reply_to_user').val('');
+			jQuery('#in_reply_to_status').val('');
+		}
+	});
+	
+		jQuery(".retweet").click(function() {
+		reply_to_status = jQuery(this).parents().filter('span.status').attr('id');
+		reply_to_user = jQuery('#'+reply_to_status+' a.name1').html();
+		tweet = jQuery('#'+reply_to_status+' div.text').html();
+		tweet = tweet.replace(/(<([^>]+)>)/ig,""); 
+		jQuery('#tweet').val('RT @'+reply_to_user+' '+tweet);
+	});
+	
+});
+
+
+/*** Shorten URLs ***/
+jQuery(document).ready(function() {
+
+	jQuery("#shorten-url").click(function() {
+		theurl = prompt("<?php _e('Enter the URL to be shortened', 'wp-twitter') ?>", "http://");
+		if (theurl != null && theurl != "" && theurl != "http://" && theurl != false) {
+			loading = setInterval(function() {
+	     		jQuery('#shorten-url').toggleClass('tw-hide');
+			}, 500);
+			post_to = jQuery('#post_to').val();
+			datastring = 'theurl=' + theurl + '&do=shorten-url';
+			jQuery.ajax({
+				type: "POST",
+				url: post_to,
+				data: datastring,
+				success: function(data) {
+					jQuery('#tweet').val(jQuery('#tweet').val()+' '+data);
+					clearInterval(loading);
+					jQuery('#shorten-url').removeClass('tw-hide');
+				}
+			});
+		}
+	});
+
+});
+
+
+
