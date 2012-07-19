ffcb98d5a277268f96cf7c62e90e44134eb8d658
fdoromo <fdoromo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-19 03:51:38 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wp-twitter/tags/3.1@574381 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/fdx-inc.js
@@ -0,0 +1,34 @@
+/* popup normal para share
+-------------------------------------------------------------- */
+function PopupCenter(pageURL, title,w,h,scrol) {
+var left = (screen.width/2)-(w/2);
+var top = (screen.height/2)-(h/2);
+var targetWin = window.open (pageURL, title, 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars='+scrol+', resizable=no, copyhistory=no, width='+w+', height='+h+', top='+top+', left='+left);
+}
+
+/*"Accept terms" form submission- By Dynamic Drive
+-------------------------------------------------------------- */
+var checkobj
+
+function agreesubmit(el){
+checkobj=el
+if (document.all||document.getElementById){
+for (i=0;i<checkobj.form.length;i++){  //hunt down submit button
+var tempobj=checkobj.form.elements[i]
+if(tempobj.type.toLowerCase()=="submit")
+tempobj.disabled=!checkobj.checked
+}
+}
+}
+
+function defaultagree(el){
+if (!document.all&&!document.getElementById){
+if (window.checkobj&&checkobj.checked)
+return true
+else{
+alert("Please read/accept terms to submit form")
+return false
+}
+}
+}
+
