3fa82385746dee7d8aabd92ddafd289e9ba0423b
fdoromo <fdoromo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-13 22:35:36 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wp-twitter/tags/3.5@612085 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/fdx-inc.js
@@ -0,0 +1,51 @@
+/* popup normal para share
+-------------------------------------------------------------- */
+function PopupCenter(pageURL, title,w,h,scrol) {
+var left = (screen.width/2)-(w/2);
+var top = (screen.height/2)-(h/2);
+var targetWin = window.open (pageURL, title, 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars='+scrol+', resizable=no, copyhistory=no, width='+w+', height='+h+', top='+top+', left='+left);
+}
+
+/*"Accept terms" form submission- By Dynamic Drive
+-------------------------------------------------------------- */
+var checkobj
+
+function agreesubmit(el){
+checkobj=el
+if (document.all||document.getElementById){
+for (i=0;i<checkobj.form.length;i++){  //hunt down submit button
+var tempobj=checkobj.form.elements[i]
+if(tempobj.type.toLowerCase()=="submit")
+tempobj.disabled=!checkobj.checked
+}
+}
+}
+
+function defaultagree(el){
+if (!document.all&&!document.getElementById){
+if (window.checkobj&&checkobj.checked)
+return true
+else{
+alert("Please read/accept terms to submit form")
+return false
+}
+}
+}
+/* Shortcodes tabs--------------------------------  */
+jQuery(document).ready(function($){
+	//Default Action
+	$(".tab_content").hide(); //Hide all content
+	$("ul.tabs li:first").addClass("active").show(); //Activate first tab
+	$(".tab_content:first").show(); //Show first tab content
+
+	//On Click Event
+	$("ul.tabs li").click(function() {
+		$("ul.tabs li").removeClass("active"); //Remove any "active" class
+		$(this).addClass("active"); //Add "active" class to selected tab
+		$(".tab_content").hide(); //Hide all tab content
+		var activeTab = $(this).find("a").attr("href"); //Find the rel attribute value to identify the active tab + content
+		$(activeTab).fadeIn(); //Fade in the active content
+		return false;
+	});
+
+});
