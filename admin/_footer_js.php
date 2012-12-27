d04e12029539e8b0a9df2b3e358c43efae0b87f2
fdoromo <fdoromo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-27 12:41:23 +0000

    tags/3.7.2
    git-svn-id: https://plugins.svn.wordpress.org/wp-twitter/tags/3.7.2@645008 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin/_footer_js.php
@@ -0,0 +1,25 @@
+<script type="text/javascript" src="<?php echo admin_url(); ?>load-scripts.php?c=0&amp;load=jquery-ui-core,jquery-ui-widget,jquery-ui-mouse,jquery-ui-sortable,postbox,post"></script>
+<script language="JavaScript" type="text/javascript">
+function PopupCenter(pageURL, title,w,h,scrol) {
+var left = (screen.width/2)-(w/2);
+var top = (screen.height/2)-(h/2);
+var targetWin = window.open (pageURL, title, 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars='+scrol+', resizable=no, copyhistory=no, width='+w+', height='+h+', top='+top+', left='+left);
+}
+//reset
+jQuery(document).ready(function($) {
+$("#fdxReset").submit(function(event) {
+var ask = confirm('<?php _e('Are you sure you want to reset all settings?', 'fdx-lang') ?>');
+if (!ask) {
+event.preventDefault();
+return false;
+}
+   });
+  });
+//alert
+$(document).ready(function(){
+$("#cl").click(function(){
+alert("fabrix@fabrix.net");
+});
+});
+
+</script>
